84cf9d2ca9312627fe05af7f10e435b619cb3452
1xotab <ninetonine319@gmail.com>
2021-12-24 21:04:18 +0300

    Spaces have deleted Junit version is variable First test (null) is working

--- src/test/java/com/Foxminded/Anagrams/AnagramsTest.java
+++ src/test/java/com/Foxminded/Anagrams/AnagramsTest.java
@@ -1,69 +1,68 @@
 package com.Foxminded.Anagrams;
 
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class TestAnagrams {
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+class TestAnagrams {
     Anagrams swapper = new Anagrams();
 
     @Test
-    public void swapLettersInTheWords_shouldThrowNullPointerException_whenInputStringIsNull() {
-        String actual = swapper.swapLettersInTheWords(null);
-        Assertions.assertEquals(null, actual);
+    void swapLettersInTheWords_shouldThrowNullPointerException_whenInputStringIsNull() {
+        assertThrows(NullPointerException.class, () -> swapper.swapLettersInTheWords(null));
     }
 
     @Test
-    public void swapLettersInTheWords_shouldReturnEmptyString_whenInputStringIsEmpty() {
+    void swapLettersInTheWords_shouldReturnEmptyString_whenInputStringIsEmpty() {
         String actual = swapper.swapLettersInTheWords("");
         Assertions.assertEquals("", actual);
     }
 
     @Test
-    public void swapLettersInTheWords_shouldReturnSingleSpace_whenInputStringIsSingleSpace() {
+    void swapLettersInTheWords_shouldReturnSingleSpace_whenInputStringIsSingleSpace() {
         String actual = swapper.swapLettersInTheWords(" ");
         Assertions.assertEquals(" ", actual);
     }
 
     @Test
-    public void swapLettersInTheWords_shouldReturnSeveralSpace_whenInputStringConsistOfSeveralSpaces() {
+    void swapLettersInTheWords_shouldReturnSeveralSpace_whenInputStringConsistOfSeveralSpaces() {
         String actual = swapper.swapLettersInTheWords("   ");
         Assertions.assertEquals("   ", actual);
     }
 
     @Test
-    public void swapLettersInTheWords_shouldReturnSingleCharacter_whenInputStringIsSingleCharacter() {
+    void swapLettersInTheWords_shouldReturnSingleCharacter_whenInputStringIsSingleCharacter() {
         String actual = swapper.swapLettersInTheWords("a");
         Assertions.assertEquals("a", actual);
     }
 
     @Test
-    public void swapLettersInTheWords_shouldReturnMultiSameLetterString_whenInputStringIsMultiSameLetter() {
+    void swapLettersInTheWords_shouldReturnMultiSameLetterString_whenInputStringIsMultiSameLetter() {
         String actual = swapper.swapLettersInTheWords("aaaa");
         Assertions.assertEquals("aaaa", actual);
     }
 
     @Test
-    public void swapLettersInTheWords_shouldReturnSwappedSameCharacterInLowerAndUpperCasesMixed_whenInputStringSameCharacterInLowerAndUpperCasesMixed() {
+    void swapLettersInTheWords_shouldReturnSwappedSameCharacterInLowerAndUpperCasesMixed_whenInputStringSameCharacterInLowerAndUpperCasesMixed() {
         String actual = swapper.swapLettersInTheWords("aAaAaA");
         Assertions.assertEquals("AaAaAa", actual);
     }
 
     @Test
-    public void swapLettersInTheWords_shouldReturnWordWithSwappedLetters_whenInputStringWordWithDifferentLetters() {
+    void swapLettersInTheWords_shouldReturnWordWithSwappedLetters_whenInputStringWordWithDifferentLetters() {
         String actual = swapper.swapLettersInTheWords("gate");
         Assertions.assertEquals("etag", actual);
     }
 
     @Test
-    public void swapLettersInTheWords_shouldReturnSymbolsInTheSameOrder_whenInputStringConsistOfSymbols() {
+    void swapLettersInTheWords_shouldReturnSymbolsInTheSameOrder_whenInputStringConsistOfSymbols() {
         String actual = swapper.swapLettersInTheWords("!+-0343&^^^^%%");
         Assertions.assertEquals("!+-0343&^^^^%%", actual);
     }
 
     @Test
-    public void swapLettersInTheWords_shouldReturnEachWordWithSwappedLetters_whenInputStringConsistOfSeveralWords() {
+    void swapLettersInTheWords_shouldReturnEachWordWithSwappedLetters_whenInputStringConsistOfSeveralWords() {
         String actual = swapper.swapLettersInTheWords("word gate");
         Assertions.assertEquals("drow etag", actual);
     }
